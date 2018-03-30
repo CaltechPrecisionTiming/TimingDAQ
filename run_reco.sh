c1c7cee8b5e782581fb11b3db848d9c6d6e627bf
Olmo Cerri <olmo.cerri@gmail.com>
2018-03-30 17:41:29 -0500

    Run reco modified

--- run_reco.sh
+++ run_reco.sh
@@ -12,8 +12,9 @@ for((runNum=${numberlo}; runNum<=${numberhi}; runNum++))
   echo "processing run number " ${runNum}
   echo "copying Raw VME data to lpc eos"
   rsync -artv --progress otsdaq@ftbf-daq-08.fnal.gov:/data/TestBeam/2018_03_March_CMSTiming/CMSTiming/RawDataSaver0CMSVMETiming_Run${runNum}_*_Raw.dat $data_dir/
-  nfiles=$(ls /eos/uscms/store/user/cmstestbeam/BTL/March2018/OTSDAQ/CMSTiming/RawDataSaver0CMSVMETiming_Run${runNum}_*.dat | wc -l)
+  nfiles=$(ls ${data_dir}/RawDataSaver0CMSVMETiming_Run${runNum}_*.dat | wc -l)
   echo "number of raw DRS files: ${nfiles}"
+  echo
   if [ $nfiles -gt 1 ]
   then
     echo "Combining .dat files"
@@ -21,13 +22,14 @@ for((runNum=${numberlo}; runNum<=${numberhi}; runNum++))
     then
     	echo -e "${RED}Combined .dat file already exists.${NC} Delete it first if you would like to recombine."
     	echo "Filename: $data_dir/RawDataSaver0CMSVMETiming_Run${runNum}_combined.dat"
+      exit 1
     else
     	cat $(ls -v $data_dir/RawDataSaver0CMSVMETiming_Run${runNum}_*.dat) > $data_dir/RawDataSaver0CMSVMETiming_Run${runNum}_combined.dat
     	echo "Created $data_dir/RawDataSaver0CMSVMETiming_Run${runNum}_combined.dat"
     fi
     flag=combined
   else
-    flag=$(ls /eos/uscms/store/user/cmstestbeam/BTL/March2018/OTSDAQ/CMSTiming/RawDataSaver0CMSVMETiming_Run110_*.dat | tail -c 10 | head -c 1)
+    flag=$(ls /eos/uscms/store/user/cmstestbeam/BTL/March2018/OTSDAQ/CMSTiming/RawDataSaver0CMSVMETiming_Run110_*.dat | tail -c 10 | head -c 5)
   fi
 
   echo $flag
@@ -35,7 +37,7 @@ for((runNum=${numberlo}; runNum<=${numberhi}; runNum++))
   echo "Recostructing VME and merging pixel data:"
   echo "$code_dir/VMEDat2Root --input_file=$data_dir/RawDataSaver0CMSVMETiming_Run${runNum}_${flag}_Raw.dat --pixel_input_file=$data_dir/Run${runNum}_CMSTiming_converted.root --output_file=$data_dir/RECO/V1/RawDataSaver0CMSVMETiming_Run${runNum}.root --config=$code_dir/config/VME_FNALTestbeam_180329_v1.config"
 
-  $code_dir/VMEDat2Root --input_file=$data_dir/RawDataSaver0CMSVMETiming_Run${runNum}_${flag}_Raw.dat --pixel_input_file=$data_dir/Run${runNum}_CMSTiming_converted.root --output_file=$data_dir/RECO/V1/RawDataSaver0CMSVMETiming_Run${runNum}.root --config=$code_dir/config/VME_FNALTestbeam_180329_v1.config
+  $code_dir/VMEDat2Root --input_file=$data_dir/RawDataSaver0CMSVMETiming_Run${runNum}_${flag}.dat --pixel_input_file=$data_dir/Run${runNum}_CMSTiming_converted.root --output_file=$data_dir/RECO/V1/RawDataSaver0CMSVMETiming_Run${runNum}.root --config=$code_dir/config/VME_FNALTestbeam_180329_v1.config
 
   echo "finish with processing run number " ${runNum}
 }
