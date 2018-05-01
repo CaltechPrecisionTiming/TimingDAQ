41af0524182adef6afc120735cfbdb031065f639
Si Xie <si.xie@cern.ch>
2018-04-30 22:11:08 -0500

    update to v2

--- automation/run_reco.sh
+++ automation/run_reco.sh
@@ -72,7 +72,7 @@ data_dir=/eos/uscms/store/user/cmstestbeam/ETL/2018_04/OTSDAQ/CMSTiming/VME/RAW
 #data_dir=/tmp/zhicai/
 output_name=DataCMSVMETiming
 #output_dir=../data/ETL_march_testbeam/RECO_v1
-output_dir=$data_dir/../RECO/v1
+output_dir=$data_dir/../RECO/v2
 code_dir=.
 config_file=$code_dir/config/VME_FNALTestbeam_180409_LGAD_v1.config
 
