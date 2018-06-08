315b34388e000d9a9559b92bf76bdef8deff4c2f
Olmo Cerri <olmo.cerri@gmail.com>
2018-06-08 12:36:08 -0500

    cmslpc setup

---
+++ setup_cmslpc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /cvmfs/sft.cern.ch/lcg/views/LCG_89/x86_64-slc6-gcc62-opt/setup.sh
+source  /cvmfs/sft.cern.ch/lcg/releases/ROOT/6.10.02-19565/x86_64-slc6-gcc62-opt/bin/thisroot.sh
