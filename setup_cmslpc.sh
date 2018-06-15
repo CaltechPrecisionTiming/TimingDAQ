c05109352bf898c6b5395972e5f5310ff9352c28
Si Xie <si.xie@cern.ch>
2018-06-14 20:43:03 -0500

    update cmslpc setup script

--- setup_cmslpc.sh
+++ setup_cmslpc.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# source /cvmfs/sft.cern.ch/lcg/views/LCG_89/x86_64-slc6-gcc62-opt/setup.sh
-# source  /cvmfs/sft.cern.ch/lcg/releases/ROOT/6.10.02-19565/x86_64-slc6-gcc62-opt/bin/thisroot.sh
-
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_88/gcc/4.9.3/x86_64-slc6/setup.sh
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_88/Python/2.7.13/x86_64-slc6-gcc49-opt/Python-env.sh
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_88/ROOT/6.08.06/x86_64-slc6-gcc49-opt/bin/thisroot.sh
