9b18367ef64b5acb93a1abda169c7d7a3b93cbd2
cmorgoth <cristian.pena@caltech.edu>
2018-07-23 10:30:30 -0700

    include interpolation and time over threshold (adding missing files)

---
+++ include/Interpolator.hh
@@ -0,0 +1,37 @@
+/*
+The Interpolator class can be used to interpolate a sampled signal using the Sampling Theorem.
+The samples are assumed to be taken at constant frequency
+(For an explanation of the Sampling Theorem see for example
+National Semiconductor Application Note 236, January 1980)
+This is From Luciano Ristori
+*/
+
+
+//
+// my implementation of sinc(x) = sin(x)/x
+//
+
+#ifndef INTERPOLATOR_HH
+#define INTERPOLATOR_HH
+
+#include <stdlib.h>
+
+class Interpolator {
+	public:
+		unsigned nSamples; // total number of samples
+		float* samples; // pointer to the array with sampled values
+		double tMin, tMax; // times of first and last samples
+
+		double deltaT; // sampling time interval
+		const int sampleRange = 64; // range of samples to use for interpolation
+		double cutFreqRel = 1.0; // cut frequency relative to max frequency (half of sampling freq)
+
+		Interpolator(){}; // default constructor
+		// initialize the interpolator with sampled data
+		double Sinc(double x);
+		void init(unsigned _nSamples, double _tMin, double _tMax, float* _samples);
+		// return interpolated value for a generic time t within [tMin, tMax]
+		double f(double t);
+};
+
+#endif
---
+++ src/Interpolator.cc
@@ -0,0 +1,44 @@
+#include "Interpolator.hh"
+#include <TMath.h>
+
+void Interpolator::init(unsigned _nSamples,double _tMin, double _tMax, float* _samples){
+  nSamples = _nSamples; // total number of samples
+  tMin = _tMin; // time of first sample
+  tMax = _tMax; // time of last sample
+  samples = _samples;	// pointer to the array with sampled values
+
+  deltaT = (tMax - tMin)/(double)(nSamples - 1); // sampling time interval
+};
+
+// return interpolated value for a generic time t within [tMin, tMax]
+
+double Interpolator::f(double t){
+  if(t < tMin) t = tMin;
+  if(t > tMax) t = tMax;
+  double s = (t - tMin)/deltaT;
+
+  int iSampleMin = (int)s - sampleRange;
+  if(iSampleMin < 0) iSampleMin = 0;
+  int iSampleMax = (int)s + sampleRange;
+  if(iSampleMax > (int)nSamples) iSampleMax = nSamples;
+
+  double result = 0.;
+  for(unsigned iSample = iSampleMin; (int)iSample != iSampleMax; ++ iSample){
+    double sNorm = (s - (double)iSample)*TMath::Pi()*cutFreqRel;
+    if(sNorm == 0.) result += samples[iSample];
+    //else result += samples[iSample]*sin(sNorm)/sNorm;
+    else result += samples[iSample]*Sinc(sNorm);
+  }
+  return result;
+};
+
+double Interpolator::Sinc(double x){
+
+	const double a2 = -1./6.;
+	const double a4 = +1./120.;
+	const double a6 = -1./5040.;
+
+	if(abs(x) > 0.001) return sin(x)/x;
+	double x2 = x*x;
+	return ((a6*x2 + a4)*x2 + a2)*x2 +1; // Taylor expansion of sinc(x) function
+};
