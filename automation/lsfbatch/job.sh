c2fa6417ddc71a7bf7d01a8c6b959c223e996358
Olmo Cerri <olmo.cerri@gmail.com>
2018-11-11 17:12:33 +0100

    lsfjob structure

--- automation/lsfbatch/job.sh
+++ automation/lsfbatch/job.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 run_number=$1
-config_version=v2ct
-echo "============= Processing run $1 ===================="
+config_version=$2
+data_dir=/eos/cms/store/group/phys_susy/razor/FNAL_TB_1811/data
+
+echo "============= Job for run $1 ===================="
 cd /eos/cms/store/group/phys_susy/razor/FNAL_TB_1811/CMSSW_9_0_2/src
 echo $PWD
 eval `scramv1 runtime -sh`
-cd /eos/cms/store/group/phys_susy/razor/FNAL_TB_1811/TimingDAQ
+cd TimingDAQ
 echo $PWD
-# source setup_lxplus.sh
-echo "Setup ok"
-python automation/DecodeData.py --vVME $config_version --NO_save_meas -R $run_number
+
+logfile=automation/lsfbatch/log/$run_number.log
+
+python automation/DecodeData.py --vVME $config_version --NO_save_meas -R $run_number --data_dir=$data_dir &> $logfile
--- automation/lsfbatch/submit_job.sh
+++ automation/lsfbatch/submit_job.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 run_number=$1
+config_version=v2ct
 
 dir=/eos/cms/store/group/phys_susy/razor/FNAL_TB_1811/TimingDAQ/automation/lsfbatch
 
-bsub -q 8nm -J decode_$run_number $dir/job.sh $run_number
+bsub -q 1nh -J decode_$run_number $dir/job.sh $run_number $config_version
 
 echo
 echo 'Done'
