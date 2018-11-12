f03d69cc26d9c5f741be3df0c48f937c0b1bbd3e
fqnet1 <fqnet1@fqnet1s-mini.guest-network.net>
2018-11-11 18:35:09 -0600

    Mac Mini data stream

---
+++ automation/transfer_ftbf2mac-mini.py
@@ -0,0 +1,108 @@
+from glob import glob
+import shutil, os, argparse
+import subprocess
+import time, re
+
+#RN is a wildcard for run run number
+VME_file_template = 'CMSTiming/RawDataSaver0CMSVMETiming_RunRN_*_Raw.dat'
+NimPlus_file_template = 'NimPlus/TriggerCountNimPlus*_RN.cnt'
+
+def GetCommandLineArgs():
+    p = argparse.ArgumentParser()
+    p.add_argument('-R', '--runs', type=int, nargs='+', default=None)
+
+    p.add_argument('--dir_ftbf', type=str, default='/data/TestBeam/2018_11_November_CMSTiming/')
+    p.add_argument('--dir_data', type=str, default='../data/')
+
+    p.add_argument('--max_void', type=int, default=-1)
+    p.add_argument('--sleep', type=float, default=60)
+    p.add_argument('--min_file_age', type=float, default=90)
+
+    p.add_argument('-v', '--verbose', action='store_true', default=False)
+
+    return p.parse_args()
+
+def transfer(args, runs):
+    for rn in runs:
+        print '============= Processing run ' + str(rn) + ' ============='
+        cmd = 'rsync -artu --progress otsdaq@ftbf-daq-08:' + args.dir_ftbf
+
+        cmd_VME = cmd + VME_file_template.replace('RN', str(rn))
+        cmd_VME += ' ' + args.dir_data + 'VME/RAW/'
+        print cmd_VME
+        subprocess.call(cmd_VME, shell=True)
+
+        cmd_NimPlus = cmd + NimPlus_file_template.replace('RN', str(rn))
+        cmd_NimPlus += ' ' + args.dir_data + 'NimPlus/'
+        print cmd_NimPlus
+        subprocess.call(cmd_NimPlus, shell=True)
+
+def get_last_remote_file(args):
+    cmd = "ssh otsdaq@ftbf-daq-08 \'cd " + args.dir_ftbf + "; "
+    cmd += "ls -1ltr " + VME_file_template.replace('RN', '*') + "\'"
+
+    output = subprocess.check_output(cmd, shell=True)
+    last_file = output.split('\n')[-2]
+    last_file = last_file.split(' ')
+    fn = last_file[-1]
+
+    size = int(last_file[-5])
+    run_number = re.search('_Run[0-9]+_', fn).group(0)[4:-1]
+    return int(run_number), size
+
+if __name__ == '__main__':
+    args = GetCommandLineArgs()
+
+    if not args.dir_ftbf.endswith('/'):
+        args.dir_ftbf += '/'
+    if not args.dir_data.endswith('/'):
+        args.dir_data += '/'
+
+    if args.runs==None:
+        last_high_run_number = 0
+        last_size = 0
+        nothing_changed = 0
+
+        last_run_number, last_size = get_last_remote_file(args)
+        print 'Current last remote run: {} ({:.1f} MB)'.format(last_run_number, last_size/float(8*1024*1024))
+        print 'Starting the loop in 5 s'
+        time.sleep(5)
+
+        while(args.max_void < 0 or nothing_changed < args.max_void):
+            high_run_number, size = get_last_remote_file(args)
+            if last_high_run_number == high_run_number and size > last_size:
+                last_size = size
+                if args.verbose:
+                    print 'Last remote file possibly increasing in size, considering previous one.'
+                high_run_number -= 1
+            elif last_high_run_number != high_run_number:
+                last_high_run_number == high_run_number
+                last_size = size
+                if args.verbose:
+                    print 'Last remote file possibly increasing in size, considering previous one.'
+                high_run_number -= 1
+
+            latest_file = glob(args.dir_data + 'VME/RAW/*')[-1]
+            run_number_local = re.search('_Run[0-9]+', latest_file).group(0)[4:]
+            run_number_local = int(run_number_local)
+
+            run_list = range(run_number_local+1, high_run_number+1)
+
+            if len(run_list) > 0:
+                print 'Transfering runs', run_list
+                transfer(args, run_list)
+                nothing_changed = 0
+            else:
+                print 'Nothing to be done'
+                nothing_changed += 1
+
+            print '\n...Going to sleep for {0:.0f} s'.format(args.sleep)
+            time.sleep(args.sleep)
+
+        print '\n\nStopped because nothing changed for at least {0:.1f} min'.format(args.sleep*nothing_changed/60.0)
+
+    else:
+        if len(args.runs) == 2 and (args.runs[0] < args.runs[1]):
+            runs_list = range(args.runs[0], args.runs[1]+1)
+
+        transfer(args, args.runs)
---
+++ automation/transfer_rulinux042mac-mini.py
@@ -0,0 +1,74 @@
+from glob import glob
+import shutil, os, argparse
+import subprocess
+import time, re
+
+#RN is a wildcard for run run number
+track_file_template = 'RunRN_CMSTiming_converted.root'
+
+def GetCommandLineArgs():
+    p = argparse.ArgumentParser()
+    p.add_argument('-R', '--runs', type=int, nargs='+', default=None)
+
+    p.add_argument('--dir_rulinux04', type=str, default='/data/TestBeam/2018_11_November_CMSTiming/CMSTimingConverted/')
+    p.add_argument('--dir_data', type=str, default='../data/')
+
+    p.add_argument('--max_void', type=int, default=-1)
+    p.add_argument('--sleep', type=float, default=30)
+    p.add_argument('--min_file_age', type=float, default=90)
+
+    p.add_argument('-f', '--force', action='store_true', default=False)
+
+    return p.parse_args()
+
+def transfer(args, rn):
+    print '============= Processing run ' + str(rn) + ' ============='
+
+    cmd = 'rsync -artu --progress otsdaq@rulinux04.dhcp.fnal.gov:'
+    cmd += args.dir_rulinux04
+    cmd += track_file_template.replace('RN', str(rn))
+    cmd += ' ' + args.dir_data + 'Tracks/'
+
+    subprocess.call(cmd, shell=True)
+
+if __name__ == '__main__':
+    args = GetCommandLineArgs()
+
+    if not args.dir_rulinux04.endswith('/'):
+        args.dir_rulinux04 += '/'
+    if not args.dir_data.endswith('/'):
+        args.dir_data += '/'
+
+    if args.runs==None:
+        last_run_number = 0
+        nothing_changed = 0
+
+        while(args.max_void < 0 or nothing_changed < args.max_void):
+            transfer(args, '*')
+
+            files = args.dir_data + 'Tracks/' + track_file_template.replace('RN', '*')
+            files = glob(files)
+            if len(files) == 0:
+                print 'No run presents.... move one manually'
+                exit(0)
+
+            run_number = re.search('Run[0-9]+_', files[-1]).group(0)[3:-1]
+
+            print 'Last run present', run_number
+
+            if last_run_number != run_number:
+                last_run_number = run_number
+            else:
+                nothing_changed += 1
+
+            print '\n\n...Going to sleep for {0:.0f} s'.format(args.sleep)
+            time.sleep(args.sleep)
+
+        print '\n\nStopped because nothing changed for at least {0:.1f} min'.format(args.sleep*nothing_changed/60.0)
+
+    else:
+        if len(args.runs) == 2 and (args.runs[0] < args.runs[1]):
+            runs_list = range(args.runs[0], args.runs[1]+1)
+
+        for rn in args.runs:
+            transfer(args, rn)
